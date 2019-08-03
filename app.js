f764e5171cebbb2727ab9d281827ecb05d67bfd8
Yvee1 <stevenvdb@live.nl>
2019-08-03 23:45:53 +0200

    Add link to recursive text and better transition

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 (function() {  
-
   // Create HTML space and form
   Pts.namespace( this );
   let space = new HTMLSpace("#pt").setup({bgcolor: "#000", resize: true });
   let form = space.getForm();
-
-  // css for testing
-  let css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".pts-text {line-height: 55vh; overflow: hidden; pointer-events: all; font-size: 15vw; transition: font-size 1s;} .pts-text:hover {font-size: 20vw}";
-  document.body.appendChild(css);
 
   function recurseMe() {
     space.clear();
@@ -21,15 +14,16 @@
 
     for (let i = 0; gap * Math.sqrt(i) <= width; i++){
       let x = gap*Math.sqrt(i);
-      let y = space.size.y / 5 + 3 * i * Math.sin(x/50);
+      let y = 5 * space.size.y / 12 + 3 * i * Math.sin(x/50);
 
       locations.push({x: x, y: y});
     }
 
     for (let i = locations.length - 1; i >= 0; i--){
-      form.strokeOnly("rgba(0, 0, 0, 0)", 1).fillText(`rgba(${255-i*10}, ${255-i*5}, ${255-i*20})`);
-      form.text([locations[i].x, locations[i].y], "me");
-      console.log([locations[i].x, locations[i].y]);
+      form.strokeOnly("rgba(0, 0, 0, 0)", 1).fillText(`rgb(${255-i*10}, ${255-i*5}, ${255-i*20})`);
+      form.styleTo("opacity", 1-(0.2+0.8*(i+1)/locations.length));
+      let text = form.text([locations[i].x, locations[i].y], "me");
+      console.log(text);
     }
   }
 
