fb770ff9eb47d9a4a650793903879f6c7226c3ad
Yvee1 <stevenvdb@live.nl>
2019-08-12 21:01:10 +0200

    better layout

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 
   const fonts = ["Arial, sans-serif", "Helvetica, sans-serif", "Times New Roman, serif", "Times, serif", "Courier New, monospace",
                 "Courier, monospace", "Verdana, sans-serif", "Georgia, serif", "Palatino,Palatino Linotype,Palatino LT STD,Book Antiqua,Georgia,serif",  
-                "Bookman", "Comic Sans MS", "Trebuchet MS", "Perpetua, serif"];
+                "Bookman", "Comic Sans MS", "Trebuchet MS", "Perpetua, serif", "Impact"];
   const fontVariants = ["normal", "normal", "small-caps"];
   const fontStyles = ["normal", "italic"];
   const fontWeight = ["normal", "bold", "bolder"];
@@ -17,6 +17,8 @@
   const offset = Math.PI/2;
   const locations = new Array(amount);
   const MEs = new Array(amount);
+
+  let count = 0;
 
   for (let i = 0; i < MEs.length; i++){
     MEs[i] = {};
@@ -36,33 +38,40 @@
       const height = space.size.y;
 
       for (let angle = 0, i = 0; angle <= 2 * Math.PI; angle += gap, i++){
-        let x = width/2 + Math.cos(-angle + offset + time/10000) * width * 0.3;
-        let y = height/2 + Math.sin(2* (-angle + offset + time/10000)) * width/8;
+        let x = width/2 + Math.cos(-angle + offset + time/1000) * width * 0.3;
+        let y = -20 + height/2 + Math.sin(2* (-angle + offset + time/1000)) * width/8;
 
         locations[i] = {x: x, y: y};
       }
 
       for (let i = locations.length - 1; i >= 0; i--){
-        form.strokeOnly("rgba(0, 0, 0, 0)", 1).fillText(`hsl(${i/locations.length * 360}, ${100-i/locations.length * 50}%, ${50+i/locations.length * 10}%)`);
+        form.strokeOnly("rgba(0, 0, 0, 0)", 1).fillText(`hsl(${50 + i/locations.length * 310}, ${100-i/locations.length * 50}%, ${50+i/locations.length * 10}%)`);
         form.alpha(0.5);
 
         form._ctx.style["font-size"] = "10vw";
+        form._ctx.style["line-height"] = "8vw";
         form._ctx.style["font-family"] = MEs[i]["font-family"];
         form._ctx.style["font-style"] = MEs[i]["font-style"];
         form._ctx.style["font-variant"] = MEs[i]["font-variant"];
         form._ctx.style["font-weight"] = MEs[i]["font-weight"];
-        form._ctx.style["z-index"] = 0;
         form.text([locations[i].x, locations[i].y], "me");
       }
 
       form._ctx.style["font-size"] = "20vw";
+      form._ctx.style["line-height"] = "14vw";
       form._ctx.style["font-family"] = "Times New Roman, serif";
       form._ctx.style["font-style"] = "italic";
       form._ctx.style["font-variant"] = "small-caps";
       form._ctx.style["font-weight"] = "lighter";
       form._ctx.style["z-index"] = 1;
+      form.fillText('red');
+      form.cls("big-me");
+
       form.alpha(0.9);
-      form.text([width/2, height/2], "me");
+      form.text([width/2, height/2 - 40], "me");
+
+      form.reset();
+      form.cls(false);
     }
   );
 
