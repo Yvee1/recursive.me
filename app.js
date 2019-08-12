76b4ba05c9370e04e15406255475b0eb3bc32ab3
Yvee1 <stevenvdb@live.nl>
2019-08-12 15:02:47 +0200

    Trying to make an actual page out of this mess

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function() {  
   // Create HTML space and form
   Pts.namespace( this );
-  let space = new HTMLSpace("#pt").setup({bgcolor: "#fff", resize: true });
+  let space = new HTMLSpace("#pt").setup({bgcolor: "#fff0", resize: true });
   let form = space.getForm();
   let test = space.getForm();
 
@@ -12,71 +12,59 @@
   const fontStyles = ["normal", "italic"];
   const fontWeight = ["normal", "bold", "bolder"];
 
-  function recurseMe() {
-    space.clear();
-    form.reset();
-    test.reset();
+  const gap = 0.7;
+  const amount = Math.ceil(2 * Math.PI / gap);
+  const offset = Math.PI/2;
+  const locations = new Array(amount);
+  const MEs = new Array(amount);
 
-    const width = space.size.x;
-    const height = space.size.y;
+  for (let i = 0; i < MEs.length; i++){
+    MEs[i] = {};
 
-    const gap = 0.7;
-    const amount = Math.floor(2 * Math.PI / gap);
-    const locations = new Array(amount);
-
-    const offset = Math.PI/2;
-
-    for (let angle = 0, i = 0; angle <= 2 * Math.PI; angle += gap, i++){
-      let x = width/2 + Math.cos(-angle + offset) * width/3;
-      let y = height/2 + Math.sin(2* (-angle + offset)) * width/8;
-
-      locations[i] = {x: x, y: y};
-    }
-
-    for (let i = locations.length - 1; i >= 0; i--){
-      form.strokeOnly("rgba(0, 0, 0, 0)", 1).fillText(`hsl(${i/locations.length * 360}, ${100-i/locations.length * 50}%, ${50+i/locations.length * 10}%)`);
-      // form.alpha(1-(0.9*(i+1)/locations.length));
-      form.alpha(0.5);
-
-      // Center text
-      //form._ctx.style['transform'] = "";
-
-      if (i == 0){
-        form._ctx.style["font-size"] = "20vw";
-        form._ctx.style["font-family"] = "Times New Roman, serif";
-        form._ctx.style["font-style"] = "italic";
-        form._ctx.style["font-variant"] = "small-caps";
-        form._ctx.style["font-weight"] = "lighter";
-        form.alpha(0.9);
-      } else {
-        form._ctx.style["font-family"] = fonts[Math.floor(Math.random()*fonts.length)];
-        form._ctx.style["font-style"] = fontStyles[Math.floor(Math.random()*fontStyles.length)];
-        form._ctx.style["font-variant"] = fontVariants[Math.floor(Math.random()*fontVariants.length)];
-        form._ctx.style["font-weight"] = fontWeight[Math.floor(Math.random()*fontWeight.length)];
-      }
-
-      // form.font(12, fontWeight[Math.floor(Math.random() * fontWeight.length)], fontStyles[Math.floor(Math.random()*fontStyles.length)], 1.2, fonts[Math.floor(Math.random()*fonts.length)]);
-      form.text([locations[i].x, locations[i].y], "me");
-
-      form.reset();
-      
-      // form.fill("red")
-      //     .circle(Circle.fromCenter([locations[i].x, locations[i].y]), 20);
-
-      form.reset();
-    }
+    MEs[i]["font-family"] = fonts[Math.floor(Math.random()*fonts.length)];
+    MEs[i]["font-style"] = fontStyles[Math.floor(Math.random()*fontStyles.length)];
+    MEs[i]["font-variant"] = fontVariants[Math.floor(Math.random()*fontVariants.length)];
+    MEs[i]["font-weight"] = fontWeight[Math.floor(Math.random()*fontWeight.length)];
   }
 
   space.add(
     // For DOM, don't use arrow function so that `this` here will refer to this player
-    {start: function(bound){
-        form.scope(this);
-        test.scope(this);
-        recurseMe();
-    }, resize: function(bound) {
-        form.scope(this);
-        test.scope(this);
-        recurseMe();
+    function (time, ftime) {
+      form.scope(this);
+
+      const width = space.size.x;
+      const height = space.size.y;
+
+      for (let angle = 0, i = 0; angle <= 2 * Math.PI; angle += gap, i++){
+        let x = width/2 + Math.cos(-angle + offset + time/10000) * width * 0.3;
+        let y = height/2 + Math.sin(2* (-angle + offset + time/10000)) * width/8;
+
+        locations[i] = {x: x, y: y};
+      }
+
+      for (let i = locations.length - 1; i >= 0; i--){
+        form.strokeOnly("rgba(0, 0, 0, 0)", 1).fillText(`hsl(${i/locations.length * 360}, ${100-i/locations.length * 50}%, ${50+i/locations.length * 10}%)`);
+        form.alpha(0.5);
+
+        form._ctx.style["font-size"] = "10vw";
+        form._ctx.style["font-family"] = MEs[i]["font-family"];
+        form._ctx.style["font-style"] = MEs[i]["font-style"];
+        form._ctx.style["font-variant"] = MEs[i]["font-variant"];
+        form._ctx.style["font-weight"] = MEs[i]["font-weight"];
+        form._ctx.style["z-index"] = 0;
+        form.text([locations[i].x, locations[i].y], "me");
+      }
+
+      form._ctx.style["font-size"] = "20vw";
+      form._ctx.style["font-family"] = "Times New Roman, serif";
+      form._ctx.style["font-style"] = "italic";
+      form._ctx.style["font-variant"] = "small-caps";
+      form._ctx.style["font-weight"] = "lighter";
+      form._ctx.style["z-index"] = 1;
+      form.alpha(0.9);
+      form.text([width/2, height/2], "me");
     }
-  });
+  );
+
+  space.play();
 })();
