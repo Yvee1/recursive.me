6a0d01523f04d03b669da429c9f3cc7b376b80d1
Yvee1 <stevenvdb@live.nl>
2019-08-15 21:58:38 +0200

    Fix some swup things

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const swup = new Swup(); // only this line when included with script tag
 
 swup.on('contentReplaced', init);
+swup.on('clickLink', linkClickHandler);
+swup.on('popState', popStateHandler);
 
 const bigMe = document.getElementById('big-me');
 
@@ -39,60 +41,78 @@ function runPt() {
     function (time, ftime) {
       form.scope(this);
 
-      if (bigMe.classList.contains("huge-me")){
-        if (window.location.pathname == '/'){
-          bigMe.classList.remove('huge-me');
+      if (!window.location.pathname == '/'){
+        space.pause();
+      } else {      
+        const width = space.size.x;
+        const height = space.size.y;
+
+        for (let angle = 0, i = 0; angle <= 2 * Math.PI; angle += gap, i++){
+          let x = width/2 + Math.cos(-angle + offset + time/1000) * width * 0.3;
+          let y = -20 + height/2 + Math.sin(2* (-angle + offset + time/1000)) * width/8;
+
+          locations[i] = {x: x, y: y};
         }
-        // console.log(window.visualViewport.width);
-        // bigMe.style.fontSize = "40vh";
-        // bigMe.style.left = `${window.innerWidth/3}px`;
-        // bigMe.style.top = `${5*window.innerHeight/12}px`;
+
+        for (let i = locations.length - 1; i >= 0; i--){
+          form.strokeOnly("rgba(0, 0, 0, 0)", 1).fillText(`hsl(${i/locations.length * 360}, ${100}%, ${50}%)`);
+
+          form.styleTo("opacity", "");
+          form._ctx.style["font-family"] = MEs[i]["font-family"];
+          form._ctx.style["font-style"] = MEs[i]["font-style"];
+          form._ctx.style["font-variant"] = MEs[i]["font-variant"];
+          form._ctx.style["font-weight"] = MEs[i]["font-weight"];
+          form.text([locations[i].x, locations[i].y], "me");
+        }
+
+        if (document.getElementById("header")){
+          document.documentElement.style.setProperty('--big-me-x', `${width/2}px`);
+          document.documentElement.style.setProperty('--big-me-y', `${height/2 - 40 + document.getElementById("header").clientHeight}px`);
+        }
       }
-
-      else if (window.location.pathname.indexOf('me.html') > -1){
-        bigMe.classList.add('huge-me');
-      }
-
-      else {
-      const width = space.size.x;
-      const height = space.size.y;
-
-      for (let angle = 0, i = 0; angle <= 2 * Math.PI; angle += gap, i++){
-        let x = width/2 + Math.cos(-angle + offset + time/1000) * width * 0.3;
-        let y = -20 + height/2 + Math.sin(2* (-angle + offset + time/1000)) * width/8;
-
-        locations[i] = {x: x, y: y};
-      }
-
-      for (let i = locations.length - 1; i >= 0; i--){
-        form.strokeOnly("rgba(0, 0, 0, 0)", 1).fillText(`hsl(${i/locations.length * 360}, ${100}%, ${50}%)`);
-
-        form.styleTo("opacity", "");
-        // form._ctx.style["font-size"] = "10vmin";
-        // form._ctx.style["line-height"] = "8vw";
-        form._ctx.style["font-family"] = MEs[i]["font-family"];
-        form._ctx.style["font-style"] = MEs[i]["font-style"];
-        form._ctx.style["font-variant"] = MEs[i]["font-variant"];
-        form._ctx.style["font-weight"] = MEs[i]["font-weight"];
-        form.text([locations[i].x, locations[i].y], "me");
-      }
-
-      document.documentElement.style.setProperty('--big-me-x', `${width/2}px`);
-      document.documentElement.style.setProperty('--big-me-y', `${height/2 - 40 + document.getElementById("header").clientHeight}px`);
-      // bigMe.style.left = `${width/2}px`;
-      // bigMe.style.top = `${height/2 - 40 + document.getElementById("header").clientHeight}px`;
-      // bigMe.style.display = "block";
-
-      // let elements = document.getElementsByClassName('pt-text');
-      // for (elements)
     }
-  }
   );
 
   space.play();
 };
 
+function linkClickHandler() {
+  console.log("Link clicked!");
+
+  if (window.location.pathname == '/'){
+    bigMe.classList.add('huge-me');
+    console.log("On index page");
+  }
+
+  if (window.location.pathname.indexOf('me.html') > -1){
+    bigMe.classList.remove('huge-me');
+    bigMe.classList.add('transition-enlarge');
+    console.log("On me.html");
+  }
+}
+
+function popStateHandler() {
+  console.log("Page change!");
+
+  if (window.location.pathname.indexOf('me.html') > -1){
+    bigMe.classList.add('huge-me');
+    console.log("Now on me.html");
+  }
+
+  if (window.location.pathname == '/'){
+    bigMe.classList.remove('huge-me');
+    bigMe.classList.add('transition-enlarge');
+    console.log("Now on index");
+  }
+}
+
 function init() {
+  console.log("init");
+
+  if (window.location.pathname.indexOf('me.html') > -1){
+    bigMe.classList.remove('transition-enlarge');
+  }
+    
   if (document.querySelector('#pt')){
     runPt();
   }
