cab5175a7a08d525ccbc9374e801560f0e90ccdb
Yvee1 <stevenvdb@live.nl>
2019-08-18 22:32:31 +0200

    More decoration

--- app.js
+++ app.js
@@ -8,76 +8,15 @@ const bigMe = document.getElementById('big-me');
 
 init();
 
-function runPt() {  
-  // Create HTML space and form
-  Pts.namespace( this );
-  let space = new HTMLSpace("#pt").setup({bgcolor: "#fff0", resize: true });
-  let form = space.getForm();
-
-  const fonts = ["Arial, sans-serif", "Helvetica, sans-serif", "Times New Roman, serif", "Times, serif", "Courier New, monospace",
-                "Courier, monospace", "Verdana, sans-serif", "Georgia, serif", "Palatino,Palatino Linotype,Palatino LT STD,Book Antiqua,Georgia,serif",  
-                "Bookman", "Comic Sans MS", "Trebuchet MS", "Perpetua, serif", "Impact"];
-  const fontVariants = ["normal", "normal", "small-caps"];
-  const fontStyles = ["normal", "italic"];
-  const fontWeight = ["normal", "bold", "bolder"];
-
-  const gap = 0.7;
-  const amount = Math.ceil(2 * Math.PI / gap);
-  const offset = Math.PI/2;
-  const locations = new Array(amount);
-  const MEs = new Array(amount);
-
-  for (let i = 0; i < MEs.length; i++){
-    MEs[i] = {};
-
-    MEs[i]["font-family"] = fonts[Math.floor(Math.random()*fonts.length)];
-    MEs[i]["font-style"] = fontStyles[Math.floor(Math.random()*fontStyles.length)];
-    MEs[i]["font-variant"] = fontVariants[Math.floor(Math.random()*fontVariants.length)];
-    MEs[i]["font-weight"] = fontWeight[Math.floor(Math.random()*fontWeight.length)];
-  }
-
-  space.add(
-    // For DOM, don't use arrow function so that `this` here will refer to this player
-    function (time, ftime) {
-      form.scope(this);
-
-      if (!window.location.pathname == '/'){
-        space.pause();
-      } else {      
-        const width = space.size.x;
-        const height = space.size.y;
-
-        for (let angle = 0, i = 0; angle <= 2 * Math.PI; angle += gap, i++){
-          let x = width/2 + Math.cos(-angle + offset + time/1000) * width * 0.3;
-          let y = -20 + height/2 + Math.sin(2* (-angle + offset + time/1000)) * width/8;
-
-          locations[i] = {x: x, y: y};
-        }
-
-        for (let i = locations.length - 1; i >= 0; i--){
-          form.strokeOnly("rgba(0, 0, 0, 0)", 1).fillText(`hsl(${i/locations.length * 360}, ${100}%, ${50}%)`);
-
-          form.styleTo("opacity", "");
-          form._ctx.style["font-family"] = MEs[i]["font-family"];
-          form._ctx.style["font-style"] = MEs[i]["font-style"];
-          form._ctx.style["font-variant"] = MEs[i]["font-variant"];
-          form._ctx.style["font-weight"] = MEs[i]["font-weight"];
-          form.text([locations[i].x, locations[i].y], "me");
-        }
-
-        if (document.getElementById("header")){
-          document.documentElement.style.setProperty('--big-me-x', `${width/2}px`);
-          document.documentElement.style.setProperty('--big-me-y', `${height/2 - 40 + document.getElementById("header").clientHeight}px`);
-        }
-      }
-    }
-  );
-
-  space.play();
-};
-
 function doMe(){
   bigMe.classList.add('huge-me');
+  if (window.innerWidth > 500){
+    document.getElementById("square").style.transform = "scale(5)";
+    document.getElementById("rotated-square").style.transform = "rotate(45deg) scale(5)";
+  } else{
+    document.getElementById("square").style.transform = "scale(10)";
+    document.getElementById("rotated-square").style.transform = "rotate(45deg) scale(10)";
+  }
   window.addEventListener('resize', positionText);
 }
 
